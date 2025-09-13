2e272db3500c9b65be70058ce614f8b627cdf338
Iaroslav Iarotskyi a01506741 <a01506741@unet.univie.ac.at>
2025-09-13 18:41:49 +0200

    General project sync

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,15 @@
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
 import sanity from '@sanity/astro';
 import { sanityConfig } from './src/utils/sanity-client';
-import flowbiteReact from "flowbite-react/plugin/astro";
 
 // https://astro.build/config
 export default defineConfig({
     image: {
         domains: ['cdn.sanity.io']
     },
-    integrations: [sanity(sanityConfig), flowbiteReact()],
+    integrations: [sanity(sanityConfig)],
     vite: {
-        plugins: [tailwindcss()],
+        plugins: [],
         server: {
             hmr: { path: '/vite-hmr/' },
             allowedHosts: ['.netlify.app']
