f80957e5a4dfc238f39cee6c9f8e95258b63dbe8
Iaroslav Iarotskyi a01506741 <a01506741@unet.univie.ac.at>
2025-09-13 14:37:03 +0200

    Url builder for Sanity platform added to the project

--- src/utils/loadQuery.ts
+++ src/utils/loadQuery.ts
@@ -2,8 +2,5 @@ import type { QueryParams } from 'sanity';
 import { client } from '@utils/sanity-client.ts';
 
 export async function loadQuery<T = any>({ query, params }: { query: string; params?: QueryParams }) {
-    console.log(query);
-    let result = await client.fetch<T>(query, params);
-    console.log(result);
-    return result;
+    return await client.fetch<T>(query, params);
 }
