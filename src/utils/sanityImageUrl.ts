f020b38a7eac6f8bd4bfc0fa77cb791b92543387
Iaroslav Iarotskyi a01506741 <a01506741@unet.univie.ac.at>
2025-09-13 16:48:47 +0200

    Image to url converter added to the project

---
+++ src/utils/sanityImageUrl.ts
@@ -0,0 +1,11 @@
+import imageUrlBuilder from '@sanity/image-url'
+import type { SanityImageSource } from '@sanity/image-url/lib/types/types'
+import { client } from '@utils/sanity-client.ts';
+
+// Create an image URL builder using the client
+const builder = imageUrlBuilder(client)
+
+// Export a function that can be used to get image URLs
+export function urlFor(source: SanityImageSource) {
+    return builder.image(source)
+}
